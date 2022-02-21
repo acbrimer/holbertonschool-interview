ec3a0de79436db56c4d0d55e8f8e989a4b0e1f77
Adam <arbimer@mac.com>
2022-02-20 21:38:20 -0600

    Updated README and added solution for task 0

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -1,13 +1,65 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - reverses a linked list of type listint_t
+ * @head: the head node of the linked list
+ *
+ * Return: new head node in reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+    listint_t *next, *prev = NULL;
+
+    if (head == NULL || *head == NULL)
+        return (NULL);
+    while (*head)
+    {
+        next = (*head)->next;
+        (*head)->next = prev;
+        prev = *head;
+        *head = next;
+    }
+    *head = prev;
+
+    return (*head);
+}
 
 /**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: pointer to the head of the linked list
- * 
+ *
  * Return: 1 if palindrome, else 0
  */
 int is_palindrome(listint_t **head)
 {
-    return (0)
+    listint_t *mid = (*head), *forward = (*head), *reverse;
+    size_t n, i;
+
+    /** get size of linked list */
+    for (n = 0; mid != NULL; n++)
+        mid = mid->next;
+    /** increment i/mid to halfway point */
+    mid = (*head);
+    for (i = 0; i < n / 2 - 1; i++)
+        mid = mid->next;
+    /** skip middle if n is odd */
+    if (n % 2 != 0)
+        mid = mid->next;
+    /** reverse nodes in list from mid */
+    reverse = reverse_listint(&mid);
+    for (i = 0; i < n / 2; i++)
+    {
+        /** if values aren't equal, cannot be palindrome */
+        if (reverse->n != forward->n)
+        {
+            reverse_listint(&mid);
+            return (0);
+        }
+        reverse = reverse->next;
+        forward = forward->next;
+    }
+    /** un-reverse nodes in list from mid */
+    reverse_listint(&mid);
+
+    return (1);
 }
---
+++ 0x04-linked_list_palindrome/run.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run \
+    -it \
+    --rm \
+    -v /Users/abrimerHome/Holberton/holbertonschool-interview/0x04-linked_list_palindrome:/home/dockerberton \
+    dockerberton:trusty
