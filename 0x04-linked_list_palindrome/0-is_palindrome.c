162e38ceabe90ea847ddcbc5f0ca4ffabb4f8c2a
Adam <arbimer@mac.com>
2022-02-20 20:31:39 -0600

    Initial commit for task 0

---
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: pointer to the head of the linked list
+ * 
+ * Return: 1 if palindrome, else 0
+ */
+int is_palindrome(listint_t **head)
+{
+    return (0)
+}
