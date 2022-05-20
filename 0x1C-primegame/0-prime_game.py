a8f3eeb09ffa6a32924d698c00224d2f4e13aaf7
Adam Brimer <abrimer@adams-mbp.lan>
2022-05-19 23:53:32 -0500

    Added solution for 0x1C

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -3,17 +3,21 @@
 0-prime_game
 """
 
+
 def isPrime(n):
     """
     isPrime - checks if a number is prime
     return: True for prime, else Flase
     """
-    if n==2 or n==3: return True
-    if n%2==0 or n<2: return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n < 2:
+        return False
     for i in range(3, int(n**0.5)+1, 2):   # only odd numbers
-        if n%i==0:
+        if n % i == 0:
             return False
     return True
+
 
 def getPrimes(n):
     """
@@ -33,7 +37,6 @@ def isWinner(x, nums):
     # handle null x/nums, x==0, empty nums
     if not (x or nums) or x == 0 or len(nums) == 0:
         return None
-    
 
     for n in range(x):
         primes = getPrimes(n)
@@ -41,9 +44,8 @@ def isWinner(x, nums):
             scores[1] += 1
         else:
             scores[0] += 1
-    
+
     if scores[0] == scores[1]:
         return None
 
     return "Maria" if scores[0] > scores[1] else "Ben"
-
